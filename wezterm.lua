ad573136082745b089d958dc6431d26fc0d4b6fe
Truong Tuan Loc <72897725+Jura69@users.noreply.github.com>
2024-11-22 12:12:34 +0700

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -7,26 +7,27 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+
 config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#47FF9C",
-	cursor_border = "#47FF9C",
-	cursor_fg = "#011423",
-	selection_bg = "#706b4e",
-	selection_fg = "#f3d9c4",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+	foreground = "#839496", -- Solarized base0
+	background = "#002b36", -- Solarized base03
+	cursor_bg = "#93a1a1", -- Solarized base1
+	cursor_border = "#93a1a1", -- Solarized base1
+	cursor_fg = "#002b36", -- Solarized base03
+	selection_bg = "#586e75", -- Solarized base01
+	selection_fg = "#fdf6e3", -- Solarized base2
+	ansi = { "#073642", "#dc322f", "#859900", "#b58900", "#268bd2", "#d33682", "#2aa198", "#eee8d5" }, -- Solarized colors
+	brights = { "#657b83", "#dc322f", "#859900", "#b58900", "#268bd2", "#d33682", "#2aa198", "#fdf6e3" }, -- Solarized bright colors
 }
 
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
-config.font_size = 19
+config.font_size = 18
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.65
-config.macos_window_background_blur = 10
+config.window_background_opacity = 1
+config.macos_window_background_blur = 5
 
 -- and finally, return the configuration to wezterm
 return config
